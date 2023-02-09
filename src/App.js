3e13f1b11c7c7d3c68225c5ae18215e8734cf97a
Camila Lopes <camilacslopes@gmail.com>
2023-02-09 11:05:29 -0300

    feat(components): create Skills component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { NavBar } from "components/NavBar.js";
+import { Banner } from "components/Banner.js";
+import { Skills } from "components/Skills.js";
+
+
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Banner />
+      <Skills />
     </div>
   );
 }
