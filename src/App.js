4dc4a795959d93b054b47f53bc4fe4b52e83544f
Camila Lopes <camilacslopes@gmail.com>
2023-02-28 08:53:54 -0300

    feat: add new components

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { NavBar } from "components/NavBar.js";
 import { Banner } from "components/Banner.js";
 import { Skills } from "components/Skills.js";
+import { Projects } from "components/Projects.js";
+import { Contact } from "components/Contact.js";
+import { Footer } from "components/Footer.js";
 
 
 
@@ -13,6 +16,9 @@ function App() {
       <NavBar />
       <Banner />
       <Skills />
+      <Projects />
+      <Contact />
+      <Footer />
     </div>
   );
 }
